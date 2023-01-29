49178439d9687f42dc9a9d6cdf5180f828b35bfe
Adam Skoczeń <adamsk91@wp.pl>
2023-01-29 10:20:25 +0100

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,9 @@
-export function fetchCountries(countryName) {
-    return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json()
-        }).catch(error => console.log("Error: ", error));
-};
+// export function fetchCountries(countryName) {
+//     return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(response.status);
+//             }
+//             return response.json()
+//         }).catch(error => console.log("Error: ", error));
+// };
