1839fd6c7fa1b4ab34e028e272a6649208f833a7
Adam Skoczeń <adamsk91@wp.pl>
2024-08-01 22:48:08 +0200

    task one complete

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,8 +1,21 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector(`input[type="text"]`);
+const startTimer = document.querySelector(`button[type="button"]`);
+const daysTimer = document.querySelector(`span[data-days]`);
+const hoursTimer = document.querySelector(`span[data-hours]`);
+const minutesTimer = document.querySelector(`span[data-minutes]`);
+const secondsTimer = document.querySelector(`span[data-seconds]`);
 
 const date = new Date();
 let userSelectedDate = 0;
+let diff = 0;
+let timerInterval;
+
+startTimer.disabled = true;
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -21,9 +34,8 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
 
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+}
 
 const options = {
   enableTime: true,
@@ -31,28 +43,46 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (date.getTime() >= selectedDates[0].getTime()) {
-      window.alert("Please choose a date in the future")
+    diff = date.getTime() >= selectedDates[0].getTime()
+    if (diff) {
+      startTimer.disabled = true;
+      iziToast.show({
+        message: "Please choose a date in the future",
+        color: 'red',
+        position: 'topRight'
+      })
     } else {
+      startTimer.disabled = false;
       userSelectedDate = selectedDates[0].getTime() - date.getTime();
     }
   }
 }
 
-const input = document.querySelector(`input[type="text"]`);
-
 flatpickr(input, options);
 
-const startTimer = document.querySelector(`button[type="button"]`);
+const addLeadingZero = (value) => {
+  return value.toString().padStart(2, "0")
+}
 
 const counter = () => {
-  console.log(userSelectedDate)
-  console.log(convertMs(userSelectedDate))
-  userSelectedDate--;
+  if (userSelectedDate < 1000) {
+    clearInterval(timerInterval);
+  }
+  userSelectedDate -= 1000;
+  const days = addLeadingZero(convertMs(userSelectedDate).days);
+  const hours = addLeadingZero(convertMs(userSelectedDate).hours);
+  const minutes = addLeadingZero(convertMs(userSelectedDate).minutes);
+  const seconds = addLeadingZero(convertMs(userSelectedDate).seconds);
+  
+  daysTimer.textContent = days;
+  hoursTimer.textContent = hours;
+  minutesTimer.textContent = minutes;
+  secondsTimer.textContent = seconds;
 }
 
 const timerOn = () => {
-  setInterval(counter, 1000);
+  startTimer.disabled = true;
+  timerInterval = setInterval(counter, 1000);
 }
 
 startTimer.addEventListener("click", timerOn);
